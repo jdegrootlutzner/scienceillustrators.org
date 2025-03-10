aa1e1dd96660276e03bc16c44930d09176ae4239
Julian DeGroot-Lutzner <jdegrootlutzner@gmail.com>
2025-03-10 09:37:50 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static', // Changed to static mode for a pre-rendered site
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
-
   adapter: cloudflare(),
 });
